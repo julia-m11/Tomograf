8ecd81e1a5c07822bb7b4aa1dee8328fcdf609c8
Tomcion <tymon.blaszkowski@gmail.com>
2026-04-23 00:11:45 +0200

    nwm

--- save_dicom.py
+++ save_dicom.py
@@ -7,24 +7,20 @@ from skimage.util import img_as_ubyte
 from skimage.exposure import rescale_intensity
 
 def save_as_dicom(file_name, img, patient_data):
-    # 1. Prepare Pixel Data
     img_rescaled = rescale_intensity(img, in_range='image', out_range=(0, 1))
     img_converted = img_as_ubyte(img_rescaled)
     pixel_bytes = img_converted.tobytes()
     if len(pixel_bytes) % 2 != 0:
         pixel_bytes += b'\x00'
 
-    # 2. File Meta Info
     file_meta = Dataset()
     file_meta.MediaStorageSOPClassUID = SecondaryCaptureImageStorage
     file_meta.MediaStorageSOPInstanceUID = generate_uid()
     file_meta.ImplementationClassUID = generate_uid()
     file_meta.TransferSyntaxUID = ExplicitVRLittleEndian 
 
-    # 3. Create Dataset
     ds = FileDataset(file_name, {}, file_meta=file_meta, preamble=b"\0" * 128)
 
-    # 4. Required Patient/Study Tags
     ds.PatientName = patient_data.get("PatientName", "Unknown^Patient")
     ds.PatientID = patient_data.get("PatientID", "000000")
     ds.ImageComments = patient_data.get("ImageComments", "000000")
@@ -36,7 +32,6 @@ def save_as_dicom(file_name, img, patient_data):
     ds.SOPClassUID = SecondaryCaptureImageStorage
     ds.Modality = "CT"
 
-    # 5. Required Image Geometry & Display Tags
     ds.SamplesPerPixel = 1
     ds.PhotometricInterpretation = "MONOCHROME2"
     ds.Rows, ds.Columns = img_converted.shape
@@ -45,7 +40,6 @@ def save_as_dicom(file_name, img, patient_data):
     ds.HighBit = 7
     ds.PixelRepresentation = 0 
     
-    # Orientation and Windowing (Fixes Infinite Loading)
     ds.ImagePositionPatient = [0, 0, 0]
     ds.ImageOrientationPatient = [1, 0, 0, 0, 1, 0]
     ds.WindowCenter = "127"
@@ -53,10 +47,8 @@ def save_as_dicom(file_name, img, patient_data):
     ds.RescaleIntercept = "0"
     ds.RescaleSlope = "1"
 
-    # 6. Set Pixel Data
     ds.PixelData = pixel_bytes
 
-    # 7. Finalize and Save
     pydicom.dataset.validate_file_meta(ds.file_meta, enforce_standard=True)
     ds.save_as(file_name, write_like_original=False)
     print(f"DICOM saved: {file_name}")
